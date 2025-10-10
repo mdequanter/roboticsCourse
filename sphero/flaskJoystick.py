f861c7f1d9e2ebcb5a2223e6f9f62310f9a32dde
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2025-10-10 15:49:50 +0200

    Update flaskJoystick.py

--- sphero/flaskJoystick.py
+++ sphero/flaskJoystick.py
@@ -125,8 +125,8 @@ class SpheroController:
                     # Besturing
                     if Y<-0.7: self.move(api,self.base_heading,self.speed)
                     elif Y>0.7: self.move(api,self.base_heading+180,self.speed)
-                    elif X>0.7: self.move(api,self.base_heading+22,speed)
-                    elif X<-0.7: self.move(api,self.base_heading-22,speed)
+                    elif X>0.7: self.move(api,self.base_heading+22,self.speed)
+                    elif X<-0.7: self.move(api,self.base_heading-22,self.speed)
                     else: api.set_speed(0)
 
                     # Heading bijhouden
