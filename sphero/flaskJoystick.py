1361433392ac6c8819d0574dc6734dde11b2e7eb
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2025-10-10 16:16:15 +0200

    Update flaskJoystick.py

--- sphero/flaskJoystick.py
+++ sphero/flaskJoystick.py
@@ -114,7 +114,7 @@ class SpheroController:
 
                     # Snelheid presets + nummerkleur opnieuw tonen
                     if self.joystick.get_button(buttons['1']):
-                        self.speed, self.color=(100,Color(255,200,0)); self.display_number(api)
+                        self.speed, self.color=(150,Color(255,200,0)); self.display_number(api)
                         self.move(api,self.base_heading,self.speed)
                     if self.joystick.get_button(buttons['2']):
                         self.speed, self.color=(150,Color(255,100,0)); self.display_number(api)
@@ -123,7 +123,7 @@ class SpheroController:
                         self.speed, self.color=(200,Color(255,50,0)); self.display_number(api)
                         self.move(api,self.base_heading,self.speed)
                     if self.joystick.get_button(buttons['4']):
-                        self.speed, self.color=(240,Color(255,0,0)); self.display_number(api)
+                        self.speed, self.color=(0,Color(255,0,0)); self.display_number(api)
                         self.move(api,self.base_heading,self.speed)
 
                     # Besturing
